23fa2b24ef21f4896de63370cfa20e64abee7510
Link3a <lina_lapa_run@inbox.ru>
2022-11-28 18:10:43 +0300

    Добавили метод area

--- main.py
+++ main.py
@@ -3,5 +3,10 @@ class Rectangle:
         self.a = a
         self.b = b
 
-main_rect = Rectangle(5, 4)
+    def area(self):
+        return self.a * self.b
 
+
+
+main_rect = Rectangle(5, 4)
+print(main_rect.area())
