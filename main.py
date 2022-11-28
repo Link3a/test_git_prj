2191b0944f882d4a6032d2fd7ef2784164dae491
Link3a <119332780+Link3a@users.noreply.github.com>
2022-11-28 18:36:09 +0300

    Update main.py
    добавили методы get_height и get_widht

--- main.py
+++ main.py
@@ -5,8 +5,10 @@ class Rectangle:
 
     def area(self):
         return self.a * self.b
-
-
+    def get_height(self):
+        return self.a
+    def get_widht(self):
+        return self.b
 
 main_rect = Rectangle(5, 4)
 print(main_rect.area())
