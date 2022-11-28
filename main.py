54770aacec11df826318e9907ed04ecfa5fdeabe
Link3a <lina_lapa_run@inbox.ru>
2022-11-28 19:09:33 +0300

    сoздали класс Circle

--- main.py
+++ main.py
@@ -10,10 +10,14 @@ class Rectangle:
     def get_widht(self):
         return self.b
 
+from math import pi
 class Circle:
     def _init_ (self, r):
         self.r = r
+    def area(self):
+        return pi * (self.r) ** 2
 
 main_rect = Rectangle(5, 4)
 print(main_rect.area())
-main_Circle(2)
+main_Circle = Circle(2)
+print(main_Circle.area())
